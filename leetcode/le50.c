8e28f0fe6399ce47b0fd4b9f203bbf05d0fbdcbc
onedayzhang <oneday.zhang@gmail.com>
2017-05-13 10:49:05 +0800

    add le50.c not test

---
+++ leetcode/le50.c
@@ -0,0 +1,17 @@
+//Implement pow(x, n).
+
+double myPow(double x, int n) {
+	if(n==1)
+	{
+		return x;
+	}
+	if(n%2==0)
+	{
+		return myPow(x,n/2)*myPow(x,n/2);
+	}
+	else
+	{
+		return x*myPow(x,int (n-1)/2)*myPow(x,(n-1)/2);
+	}
+}
+
