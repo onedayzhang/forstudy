ec3474f9f7c47ab19cda63b76d65945acaf70665
onedayzhang <oneday.zhang@gmail.com>
2017-05-15 07:04:40 -0700

    le50 ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le50.c
+++ leetcode/le50.c
@@ -3,7 +3,15 @@
 double myPow(double x, int n) {
 	if(n<0)
 	{
-		return 1/myPow(x,-n)};
+		if(n==INT_MIN)
+		{
+			return 1/x * 1/myPow(x,-n-1);
+		}
+		return 1/myPow(x,-n);
+	}
+	if(n==0)
+	{
+		return 1;
 	}
 	if(n==1)
 	{
