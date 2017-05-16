06467b156b978c30c956f8c85392a8cc903fd438
onedayzhang <oneday.zhang@gmail.com>
2017-05-16 08:06:50 -0700

    le2 ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le2.c
+++ leetcode/le2.c
@@ -15,8 +15,8 @@ struct ListNode {
  */
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 
-	struct ListNode *head;
-	struct ListNode *tmp;
+	struct ListNode *head=NULL;
+	struct ListNode *tmp=NULL;
 	int carry=0;
 	int l1_value=0;
 	int l2_value=0;
@@ -38,22 +38,47 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
 		}
 		else
 		{
+			tmp->next=(struct ListNode*)malloc(sizeof(struct ListNode));
 			tmp=tmp->next;
-			tmp=(struct ListNode*)malloc(sizeof(struct ListNode));
 		}
-		tmp->val=(l1_value+l2_value)%10;
-		carry=(l1_value+l2_value)/10;
+		tmp->val=(l1_value+l2_value+carry)%10;
+		carry=(l1_value+l2_value+carry)/10;
+		tmp->next=NULL;
+		l1_value=0;
+		l2_value=0;
 	}
 	if(carry!=0)
 	{
+		tmp->next=(struct ListNode*)malloc(sizeof(struct ListNode));
 		tmp=tmp->next;
-		tmp=(struct ListNode*)malloc(sizeof(struct ListNode));
 		tmp->val=carry;
+		tmp->next=NULL;
 	}
 	return head;
 }
 
 int main()
 {
+	struct ListNode L1,L2,L3; 
+    L1.val=7;
+	L2.val=0;
+	L3.val=4;
+	L1.next=&L2;
+	L2.next=&L3;
+	L3.next=NULL;
+	struct ListNode L4,L5,L6;
+	L4.val=4;
+	L5.val=1;
+	L6.val=6;
+	L4.next=&L5;
+	L5.next=&L6;
+	L6.next=NULL;
+    struct ListNode* L7;
+	L7=addTwoNumbers(&L1,&L4);
+	while(L7!=NULL)
+	{
+		printf("%d\n",L7->val);
+		L7=L7->next;
+	}
 	return 0;
 }
