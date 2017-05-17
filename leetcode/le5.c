5c41c96baa938d674d7f00802978a5e958c1db25
root <oneday.zhang@gmail.com>
2017-05-17 05:00:50 -0700

    le5 not ok
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le5.c
+++ leetcode/le5.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+bool checkPalindrome(char *src, int start,int end)
+{
+	if(start==end)
+	{
+		return 0;
+	}
+	while(end>start)
+	{
+		if(src[start++]!=src[end--])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+
 char* longestPalindrome(char* s) {
     
-	}
+}
