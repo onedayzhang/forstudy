2bcaa626eb6a8c6dd35886e744d73dad87c2096f
onedayzhang <oneday.zhang@gmail.com>
2017-05-18 09:04:27 -0700

    update lo7.c
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le7.c
+++ leetcode/le7.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
 int reverse(int x) {
-    
+    int sum=0;
+	while(x!=0)
+	{
+		sum=sum*10+x%10;
+		x=x/10;
 	}
+	return sum;
+}
+int main(int argc,char* argv[])
+{
+	printf("%d\n",reverse(atoi(argv[1])));
+}
