2ad2984f15aec62c7be17a4f4750836d7ab6882b
onedayzhang <oneday.zhang@gmail.com>
2017-05-19 09:09:59 -0700

    le8 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le8.c
+++ leetcode/le8.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include <limits.h>
+#include <string.h>
 int myAtoi(char* str) {
 	long long int res=0;
 	int sign=1;
 	if(str==NULL)
 		return 0;
-	if(*str=='-')
+	while(*str==' ')
 	{
-		sign=-1;
+		str++;
+	}
+	if(*str=='-'||*str=='+')
+	{
+		if(*str=='-')
+		{
+			sign=-1;
+		}
 		str++;
 	}
 	while(*str!='\0')
 	{
-		if(*str>'0'&&*str<'9')
+		if(*str>='0'&&*str<='9')
 		{
 			res=res*10+(*str-'0');
 			str++;
 		}
 		else
 		{
-			return 0;
+			break;
+		}
+		if(res*sign>INT_MAX)
+		{
+			return INT_MAX;
+		}
+		else if(res*sign<INT_MIN)
+		{
+			return INT_MIN;
 		}
 	}
-	return (res*sign>INT_MAX ||res*sign<INT_MIN)? 0:res*sign;
+	return res*sign;
+//	return (res*sign>INT_MAX ||res*sign<INT_MIN)? 0:res*sign;
 }
 
 int main(int argc ,char* argv[])
