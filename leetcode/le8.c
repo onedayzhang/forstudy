a84ffeab8f8ab5bd175e4fb8adef2914296e4363
root <oneday.zhang@gmail.com>
2017-05-19 03:04:27 -0700

    update le8
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le8.c
+++ leetcode/le8.c
@@ -1,3 +1,31 @@
+#include <stdio.h>
+#include <limits.h>
 int myAtoi(char* str) {
-    
+	long long int res=0;
+	int sign=1;
+	if(str==NULL)
+		return 0;
+	if(*str=='-')
+	{
+		sign=-1;
+		str++;
 	}
+	while(*str!='\0')
+	{
+		if(*str>'0'&&*str<'9')
+		{
+			res=res*10+(*str-'0');
+			str++;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return (res*sign>INT_MAX ||res*sign<INT_MIN)? 0:res*sign;
+}
+
+int main(int argc ,char* argv[])
+{
+	printf("%d\n",myAtoi(argv[1]));
+}
