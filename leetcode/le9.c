9727548594494dfe7e99376a17b4e0b3f16fbdb8
onedayzhang <oneday.zhang@gmail.com>
2017-05-20 19:49:55 -0700

    le9 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le9.c
+++ leetcode/le9.c
@@ -1,3 +1,22 @@
-bool isPalindrome(int x) {
-    
+
+int reverse(int x) {
+    long long int sum=0;
+	while(x!=0)
+	{
+		sum=sum*10+x%10;
+		x=x/10;
 	}
+	return (sum>INT_MAX ||sum<INT_MIN)? 0:sum;
+}
+bool isPalindrome(int x) {
+	if(x<0)
+	return 0;
+    if((reverse(x))==x)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
