711ee9ed6305aaa681f1018ecdaa42fab7f8fc57
root <oneday.zhang@gmail.com>
2017-05-22 04:17:50 -0700

    le5 is ok,but not very good
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le5.c
+++ leetcode/le5.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
+#if 0
 bool checkPalindrome(char *src, int start,int end)
 {
 	if(start==end)
@@ -15,18 +17,54 @@ bool checkPalindrome(char *src, int start,int end)
 	}
 	return 1;
 }
-
+#endif
 char* longestPalindrome(char* s) {
-    int lenth;
-	int start,int end;
-	int i;
-	if(s==NULL)
-	return NULL;
-	start=0
-	end=lenth=strlen(s);
+    int lenth,pal_len;
+	int i,j;
+	int start=0,max=1;
+	int table[1000][1000]={0};
+	lenth=strlen(s);
+	
 	for(i=0;i<lenth;i++)
 	{
-		if(checkPalindrome(start))
+		table[i][i]=1;
 	}
-
+	for(i=0;i<lenth-1;i++)
+	{
+		if(s[i]==s[i+1])
+		{
+			table[i][i+1]=1;
+			max=2;
+			start=i;
+		}
+	}
+	
+	for(pal_len=3;pal_len<=lenth;pal_len++)
+	{
+		for(i=0;i<=lenth-pal_len;i++)
+		{
+			j=i+pal_len-1;
+			if((s[i]==s[j])&&(table[i+1][j-1]==1))
+			{
+				table[i][j]=1;
+				max=pal_len;
+				start=i;
+			}
+		}
+	}
+	char *out = (char*)malloc(sizeof(char)*(max+1));
+	for(i=0;i<max;i++)
+	{
+		out[i]=s[start+i];
+	}
+	out[max]='\0';
+	return out;
 }
+
+int main(int argc, char* argv[])
+{
+	char a[100]={0};
+	strcpy(a,argv[1]);
+	printf("%s\n",longestPalindrome(a));
+}
+
