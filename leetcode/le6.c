262dbf86260f5206090ba8dc69050448b8cb39db
onedayzhang <oneday.zhang@gmail.com>
2017-05-22 08:31:46 -0700

    le8 submit fail,need check
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le6.c
+++ leetcode/le6.c
@@ -1,3 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 char* convert(char* s, int numRows) {
-    
+    int i,j,num=0;
+	int length;
+	if(s==NULL)
+	{
+		return NULL;
 	}
+	if(numRows<2)
+	{
+		return s;
+	}
+	length=strlen(s);
+	char* res =malloc(sizeof(char)*length);
+	
+	for(i=0;i<numRows;i++)
+	{
+		for(j=i;j<length;j+=2*numRows-2)
+		{
+			res[num++]=s[j];
+			if(i>0&&i<numRows-1)
+			{
+				if(j+2*numRows-2-2*i < length)
+				{
+					res[num++]=s[j+2*numRows-2-2*i];
+				}
+			}
+		}
+	}
+	return res;
+}
+
+int main(int argc,char* argv[])
+{
+	printf("%s\n",convert(argv[1],atoi(argv[2])));
+}
