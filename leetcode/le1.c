be0cfcdb3e6ccd26ead9380dcbb7d89862e00cc5
root <oneday.zhang@gmail.com>
2017-05-23 01:36:06 -0700

    add hash header file
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le1.c
+++ leetcode/le1.c
@@ -4,5 +4,10 @@
  * Note: The returned array must be malloced, assume caller calls free().
 */
 int* twoSum(int* nums, int numsSize, int target) {
-      
+      return 0;
+}
+
+int main()
+{
+	return 0;
 }
