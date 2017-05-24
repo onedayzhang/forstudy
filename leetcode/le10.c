672159d37708abbfbfe5a4d05e4ca7cda6d6c145
root <oneday.zhang@gmail.com>
2017-05-24 04:59:34 -0700

    update le10
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le10.c
+++ leetcode/le10.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 bool isMatch(char* s, char* p) {
     if(s==NULL||p==NULL)
 	{
 		return false;
 	}
-	if(*p='\0')
+	if(*p=='\0')
 	{
 		return (*s=='\0');
 	}
-	
+	if(*(p+1)!='*')
+	{
+		if(*p == *s ||*p=='.')
+		{
+			return isMatch(s+1,p+1);
+		}
+	}
+	else
+	{
+		if(*p!=*s&&*p!='.')
+		{
+			return isMatch(s,p+2);
+		}
+		else
+		{
+			return isMatch(s+1,p);
+		}
+	}
+	return false;
+}
+
+int main(int argc,char* argv[])
+{
+	printf("%d\n",isMatch(argv[1],argv[2]));
 }
