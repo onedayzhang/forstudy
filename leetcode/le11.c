2b521da99dc713656ae090a7d72e5833cea96a64
onedayzhang <oneday.zhang@gmail.com>
2017-05-25 08:24:46 -0700

    le11 is bad ,need rework
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le11.c
+++ leetcode/le11.c
@@ -1,3 +1,35 @@
+#include <stdio.h>
+
+int min(int a, int b)
+{
+	if(a>b)
+		return b;
+	return a;
+}
+
 int maxArea(int* height, int heightSize) {
-    
+    int i,j;
+	int max=0;
+	if(heightSize<2)
+	{
+		return 0;
 	}
+	for(i=0;i<heightSize-1;i++)
+	{
+		for(j=i;j<heightSize;j++)
+		{
+			if((j-i)*min(height[i],height[j])>max)
+			{
+				max=(j-i)*min(height[i],height[j]);
+			}
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	int a[5]={3,2,3,4,5};
+	printf("%d\n",maxArea(a,5));
+	return 0;
+}
