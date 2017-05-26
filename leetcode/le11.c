e69cb2a92dac587b85570045aa0f54ade0d55f02
onedayzhang <oneday.zhang@gmail.com>
2017-05-26 08:29:25 -0700

    le11 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le11.c
+++ leetcode/le11.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#if 0
 int min(int a, int b)
 {
 	if(a>b)
@@ -26,10 +26,27 @@ int maxArea(int* height, int heightSize) {
 	}
 	return max;
 }
-
+#endif
+int maxArea(int* height, int heightSize) {
+	int i=0, j=heightSize-1;
+	int maxwater=0;
+	int water,h;
+	while(i<j)
+	{
+		h=height[i]>height[j]?height[j]:height[i];
+		water=(j-i)*h;
+		if(water>maxwater)
+		{
+			maxwater=water;
+		}
+		while(height[i]<=h && i<j) i++;
+		while(height[j]<=h && i<j) j--;
+	}
+	return maxwater;
+}
 int main()
 {
-	int a[5]={3,2,3,4,5};
-	printf("%d\n",maxArea(a,5));
+	int a[8]={6,2,3,11,2,11,2,6};
+	printf("%d\n",maxArea(a,8));
 	return 0;
 }
