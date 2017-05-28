a74031ed81faf412ef8ba4f8fbcc497ac72873c3
onedayzhang <oneday.zhang@gmail.com>
2017-05-28 08:34:55 -0700

    need remove
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le19.c
+++ leetcode/le19.c
@@ -1,10 +1,31 @@
+#include <stdio.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
 /**
- * Definition for singly-linked list.
-  * struct ListNode {
-   *     int val;
-    *     struct ListNode *next;
-	 * };
-	  */
-	  struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
-	      
-		  }
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+struct ListNode* removeNthFromEnd(struct ListNode* head, int n) {
+	struct ListNode *l1,*l2;
+	l1=l2=head;
+	int i =n
+	for(;l2!=NULL&&i!=0;l2=l2->next,i--)
+	{
+		l1=l1->next;		
+	}
+	if(l1==head)
+	{
+		head=head->next;
+		free(l1);
+	}
+	else
+	{
+		
+	}
+}
