dd1116e8642f4c1f76dcd0578417d22d31091ae9
onedayzhang <oneday.zhang@gmail.com>
2017-05-29 08:02:07 -0700

    need stack function
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le20.c
+++ leetcode/le20.c
@@ -1,3 +1,31 @@
+
 bool isValid(char* s) {
-    
+	if(s==NULL)
+	return false;
+	create_stack();
+	while(s!='\0')
+	{
+		if(*s=='('||*s=='['||*s='{')
+		{
+			stack_push(*s);
+		}
+		if(*s==')')
+		{
+			if(stack_pop()!='(')
+			return false;
+		}
+		if(*s==']')
+		{
+			if(stack_pop()!='[')
+			return false;
+		}
+		if(*s=='}')
+		{
+			if(stack_pop()!='{')
+			return false;
+		}
+		s++;
 	}
+	if(stack_empty()==true)
+	return true
+}
