82a56ff9fbdf0f0c10ccf1e34bc62d55d01010fe
onedayzhang <oneday.zhang@gmail.com>
2017-05-29 08:29:07 -0700

    need check on le
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le20.c
+++ leetcode/le20.c
@@ -1,11 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+char num[1024];
+int position;
+
+int create_stack()
+{
+//	num[1024]={0};
+	position=0;
+}
+int stack_push(char src)
+{
+	num[position]=src;
+	position++;	
+}
+char stack_pop()
+{
+	char src='0';
+	if(position>=1)
+	{
+		src=num[position-1];
+		num[position]=0;
+		position--;
+	}
+	return src;
+}
+bool stack_isempty()
+{
+	return position==0;
+}
 
 bool isValid(char* s) {
 	if(s==NULL)
 	return false;
 	create_stack();
-	while(s!='\0')
+	while(*s!='\0')
 	{
-		if(*s=='('||*s=='['||*s='{')
+		if(*s=='('||*s=='['||*s=='{')
 		{
 			stack_push(*s);
 		}
@@ -26,6 +58,14 @@ bool isValid(char* s) {
 		}
 		s++;
 	}
-	if(stack_empty()==true)
-	return true
+	if(stack_isempty()==true)
+	return true;
+	else
+	return false;
+}
+
+int main(int argc ,char* argv[])
+{
+	printf("%d\n",isValid(argv[1]));
+	return 0;
 }
