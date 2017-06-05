34961305b09a9b1d8a1df617fe1d80c8d4845b46
onedayzhang <oneday.zhang@gmail.com>
2017-06-05 22:36:47 +0800

    le18 is ok

--- leetcode/le18.c
+++ leetcode/le18.c
@@ -41,61 +41,82 @@ void quick_sort(int *num,int start,int end)
 	quick_sort(num,i+1,end);
 }
 
-
-int** threeSum(int* nums, int numsSize, int* returnSize) {
-	int i,j,k;
-	int **res=NULL;
-	int size=0;
-    int total=64;
-	int n;
-	res=(int**)malloc(sizeof(int*)*total);
-    for(i = 0; i < total; ++i)
-        res[i] = (int *)malloc(sizeof(int) * 3);
-	quick_sort(nums,0,numsSize-1);
-	for(i=0;i<numsSize-2;i++)
-	{
-		if(i>0 && nums[i]==nums[i-1])
-		{
-			continue;
-		}
-		j=i+1;
-		k=numsSize-1;
-		while(j<k)
-		{
-			if((nums[i]+nums[j]+nums[k])<0)
-			{
-				j++;
-			}
-			else if((nums[i]+nums[j]+nums[k])>0)
-			{
-				k--;
-			}
-			else
-			{
-				res[size][0]=nums[i];
-				res[size][1]=nums[j];
-				res[size][2]=nums[k];
-				while(j<k && res[size][1]==nums[j])
-				j++;
-				while(j<k && res[size][2]==nums[k])
-				k--;
-				size++;
-				if(size==total)
-                {
-                    total<<=1;
-                    res=(int**)realloc(res,sizeof(int*)*total);    
-                    for(n = size; n < total; ++n)
-                        res[n] = (int *)malloc(sizeof(int) * 3);
-                }
-			}
-		}
-	}
-	*returnSize=size;
-	return res;
 /**
 * Return an array of arrays of size *returnSize.
 * Note: The returned array must be malloced, assume caller calls free().
 */
 int** fourSum(int* nums, int numsSize, int target, int* returnSize) {
+	int h,i,j,k;
+	int **res=NULL;
+	int size=0;
+	int sum=0;
+    int total=64;
+	int n;
+	res=(int**)malloc(sizeof(int*)*total);
+    for(i = 0; i < total; ++i)
+        res[i] = (int *)malloc(sizeof(int) * 4);
+	quick_sort(nums,0,numsSize-1);
+	
+	for(h=0;h<numsSize-3;h++)
+	{	
+		if(h>0 && nums[h]==nums[h-1])
+		{
+			continue;
+		}
+		for(i=h+1;i<numsSize-2;i++)
+		{
+			if(i>h+1 && nums[i]==nums[i-1])
+			{
+				continue;
+			}
+			j=i+1;
+			k=numsSize-1;
+			while(j<k)
+			{
+				if((nums[h]+nums[i]+nums[j]+nums[k]-target)<0)
+				{
+					j++;
+				}
+				else if((nums[h]+nums[i]+nums[j]+nums[k]-target)>0)
+				{
+					k--;
+				}
+				else
+				{
+					res[size][0]=nums[h];
+					res[size][1]=nums[i];
+					res[size][2]=nums[j];
+					res[size][3]=nums[k];
+					while(j<k && res[size][2]==nums[j])
+					j++;
+					while(j<k && res[size][3]==nums[k])
+					k--;
+					size++;
+					if(size==total)
+	                {
+	                    total<<=1;
+	                    res=(int**)realloc(res,sizeof(int*)*total);    
+	                    for(n = size; n < total; ++n)
+	                        res[n] = (int *)malloc(sizeof(int) * 4);
+	                }
+				}
+			}
+		}
+	}
+	*returnSize=size;
+	return res;
+}
 
+int main(int argc,char * argv [ ])
+{			  -4,-1,-1,0,1,2
+	int a[6]={-1,0,1,2,-1,-4};
+	int **ret=NULL;
+	int retsize=0;
+	ret=fourSum(a,6,0,&retsize);
+	if(retsize!=0)
+	{
+		printf("%d %d %d %d\n",ret[retsize-1][0],ret[retsize-1][1],ret[retsize-1][2],ret[retsize-1][3]);
+		retsize--;
+	}
+	return 0;
 }
