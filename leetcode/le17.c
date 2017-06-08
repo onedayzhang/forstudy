3e87b4ce75e0f19aa6013a9e169e86ec88a6098f
root <oneday.zhang@gmail.com>
2017-06-08 19:52:17 +0800

    update le17
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le17.c
+++ leetcode/le17.c
@@ -3,5 +3,22 @@
 * Note: The returned array must be malloced, assume caller calls free().
 */
 char** letterCombinations(char* digits, int* returnSize) {
-       
+
+	char *phone_num[]= { NULL,
+					     NULL,
+					     "abc",
+					     "def",
+					"ghi",
+					"jkl",
+					"mno",
+					"pqrs",
+					"tuv",
+					"wxyz",
+					NULL
+					};
+}
+
+int main()
+{
+	
 }
