33993f39c997ce05a1b72d559bd465448a807355
onedayzhang <oneday.zhang@gmail.com>
2017-06-14 00:02:15 +0800

    update le17

--- leetcode/le17.c
+++ leetcode/le17.c
@@ -1,21 +1,37 @@
+
+int backtracing(char *digits,char**phone_num,int *returnSize)
+{
+	if(*digits=='\0')
+	{
+		push('\0');
+		(*returnSize)++;
+		return 0;
+	}
+	for(;phone_num[*digits-'0']!=NULL&&*phone_num[*digits-'0']!='\0';phone_num[*digits-'0']++)
+	{
+		push(*phone_num[*digits-'0']);
+		backtracing(digits++,phone_num,)
+	}
+}
 /**
 * Return an array of size *returnSize.
 * Note: The returned array must be malloced, assume caller calls free().
 */
 char** letterCombinations(char* digits, int* returnSize) {
 
-	char *phone_num[]= { NULL,
-					     NULL,
-					     "abc",
-					     "def",
-					"ghi",
-					"jkl",
-					"mno",
-					"pqrs",
-					"tuv",
-					"wxyz",
-					NULL
-					};
+	char *phone_num[]={ NULL,
+					    NULL,
+					    "abc",
+					    "def",
+   						"ghi",
+   						"jkl",
+   						"mno",
+   						"pqrs",
+   						"tuv",
+   						"wxyz",
+   						NULL
+   						};
+	
 }
 
 int main()
