f66b0dead08849657a4a9f401caa7dfe2c5e7b68
onedayzhang <oneday.zhang@gmail.com>
2017-06-27 08:16:18 -0700

    add some
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le17.c
+++ leetcode/le17.c
@@ -1,5 +1,5 @@
 
-int backtracing(char *digits,char**phone_num,int *returnSize)
+int backtracing(char *digits,char**phone_num,int *returnSize,char **res)
 {
 	if(*digits=='\0')
 	{
@@ -10,7 +10,7 @@ int backtracing(char *digits,char**phone_num,int *returnSize)
 	for(;phone_num[*digits-'0']!=NULL&&*phone_num[*digits-'0']!='\0';phone_num[*digits-'0']++)
 	{
 		push(*phone_num[*digits-'0']);
-		backtracing(digits++,phone_num,)
+	backtracing(digits+1,phone_num,)
 	}
 }
 /**
@@ -31,6 +31,9 @@ char** letterCombinations(char* digits, int* returnSize) {
    						"wxyz",
    						NULL
    						};
+	char **res=malloc(sizeof(char)*5*4*4);
+	*returnSize=0;
+	backtracing(digits,phone_num,int * returnSize,res)
 	
 }
 
