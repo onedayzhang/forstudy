34d98f611793a3cebe98179a02a4dce831dbacc0
root <oneday.zhang@gmail.com>
2017-06-29 20:06:00 +0800

    update
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le17.c
+++ leetcode/le17.c
@@ -6,14 +6,18 @@ int backtracing(char *digits,char**phone_num,int *returnSize,int start,char **re
 	{
 		//push('\0');
 		res[*returnSize][start]='\0';
+		printf("out%s\n",res[*returnSize]);
 		(*returnSize)++;
+		
 		start=0;
 		return 0;
 	}
+	printf("digits %s\n",digits);
 	for(;phone_num[*digits-'0']!=NULL&&*phone_num[*digits-'0']!='\0';phone_num[*digits-'0']++)
 	{
 		//push(*phone_num[*digits-'0']);
 		res[*returnSize][start]=*phone_num[*digits-'0'];
+		printf("%c\n",*phone_num[*digits-'0']);
 		start++;
 		backtracing(digits+1,phone_num,returnSize,start,res);
 	}
@@ -36,7 +40,11 @@ char** letterCombinations(char* digits, int* returnSize) {
    						"wxyz",
    						NULL
    						};
-	char **res=malloc(sizeof(char)*5*4*4);
+	char **res=malloc(sizeof(char*)*4*4);
+	for(int i=0;i<4*4;i++)
+	{
+		res[i]=malloc(3*sizeof(char));
+	}
 	*returnSize=0;
 	int start=0;
 	backtracing(digits,phone_num,returnSize,start,res);
@@ -51,6 +59,7 @@ int main(int argc,char *argv[])
 	{
 		res=letterCombinations(argv[1],&returnsize);
 	}
+	printf("return %d\n",returnsize);
 	for(int i=0;i<returnsize;i++)
 	{
 		printf("%s",res[i]);
