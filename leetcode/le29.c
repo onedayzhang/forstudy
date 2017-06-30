bdde2466d6e893b7393737d5cc2d1ccd8e5edd76
onedayzhang <oneday.zhang@gmail.com>
2017-06-30 08:59:56 -0700

    update le29
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le29.c
+++ leetcode/le29.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define MAX_INT 0
 int divide(int dividend, int divisor) {
-	int res;
+	long long res=0;
+	
 	if(dividend<divisor)
+	return 0;
+	while(dividend>divisor)
+	{
+		dividend-=divisor;
+		res++;
+		if(res>INT_MAX)
+		return MAX_INT;
+	}
+}
+
+int main(int argc,char *argv[])
+{
+	printf("%d\n",divide(atoi(argv[1]),atoi(argv[2])));
 	return 0;
 }
