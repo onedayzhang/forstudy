d8b0aedee4b4518532ab505a9814df904c7f1387
root <oneday.zhang@gmail.com>
2017-06-30 16:49:50 +0800

    le27 is ok
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le27.c
+++ leetcode/le27.c
@@ -1,3 +1,36 @@
+#include <stdio.h>
 int removeElement(int* nums, int numsSize, int val) {
-    
+	int i,j;
+	int size=numsSize;
+	i=0;
+	j=numsSize-1;
+	while(i<j)
+	{
+		if(nums[j]==val)
+		{
+			size--;
+			j--;
+			continue;
+		}
+		if(nums[i]==val)
+		{
+			nums[i]=nums[j];
+			size--;
+		//	i++;
+			j--;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	if(size==1 && nums[j]==val)
+	return 0;
+	return size;
+}
+
+int main()
+{
+	int a[3]={2,3,3};
+	printf("%d\n",removeElement(a,3,3));
 }
