f43fc367ec49a7b95b245d8d7ecdd9758002b99a
onedayzhang <oneday.zhang@gmail.com>
2017-07-04 08:26:02 -0700

    start le26
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le26.c
+++ leetcode/le26.c
@@ -1,3 +1,15 @@
+#include <stdio.h>
 int removeDuplicates(int* nums, int numsSize) {
-    
+
+	int i;
+	if(numsSize<2)
+		return numsSize
+	i=0;j=1;
+	while(j<numsSize)
+	{
+		if(nums[i]==nums[j])
+		{
+			
+		}
+	}
 }
