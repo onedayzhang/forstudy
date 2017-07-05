9fc87b98679e2451adffa06a413dbbe8e2721e1f
root <oneday.zhang@gmail.com>
2017-07-05 20:00:26 +0800

    update le26
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le26.c
+++ leetcode/le26.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 int removeDuplicates(int* nums, int numsSize) {
 
-	int i;
+	int i,j;
 	if(numsSize<2)
-		return numsSize
+		return numsSize;
 	i=0;j=1;
 	while(j<numsSize)
 	{
 		if(nums[i]==nums[j])
 		{
-			
+			j++;
+		}
+		else
+		{
+			if(j-i>1)
+			{
+				nums[i+1]=nums[j];
+			}
+			i++;
 		}
 	}
+	return i;
+}
+
+int main()
+{
+	int a[10]={1,2,3,3,5,5,7,7,8,9};
+	printf("%d\n",removeDuplicates(a,10));
+	for(int i=0;i<10;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
 }
