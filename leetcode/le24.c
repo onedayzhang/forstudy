c10a2935691e8fbe353c2ca1db59ad67039a45eb
onedayzhang <oneday.zhang@gmail.com>
2017-08-03 07:38:05 -0700

    le24 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le24.c
+++ leetcode/le24.c
@@ -5,6 +5,34 @@
  *     struct ListNode *next;
  * };
  */
+#include <stdio.h>
+
+struct ListNode {
+	   int val;
+	   struct ListNode *next;
+};
+
 struct ListNode* swapPairs(struct ListNode* head) {
-    
+	struct ListNode *L1,*L2;
+	if(head==NULL)
+		return head;
+	L1=head;
+	L2=head->next;
+	while(L1!=NULL&&L2!=NULL)
+	{
+		int value=L1->val;
+		L1->val=L2->val;
+		L2->val=value;
+		L1=L2->next;
+		if(L1!=NULL)
+			L2=L1->next;
+		else
+			break;
+	}
+	return head;
+}
+
+int main()
+{
+	return 0;
 }
