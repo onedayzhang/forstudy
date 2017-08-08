36ddb68d025e3a471b80eb793474b1cd54783bed
onedayzhang <oneday.zhang@gmail.com>
2017-08-08 08:10:12 -0700

    update LIS
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- tips/DP/LIS.c
+++ tips/DP/LIS.c
@@ -4,12 +4,22 @@ int main(int argc,char argv)
 {
 	int i,j;
 	int len=0;
-	int nums[8]={23,54,12,23,54,31,24,65,26}
-	int tmp[8]
-	for(i=0;i<8i++)
+	int nums[8]={53,55,32,23,54,31,65,26};
+	int tmp[8];
+	for(i=0;i<8;i++)
 	{
 		tmp[i]=1;
-		for(j=)
+		for(j=0;j<i;j++)
+		{
+			if((nums[j]<=nums[i])&&tmp[i]<tmp[j]+1)
+			{
+				tmp[i]=tmp[j]+1;
+			}
+		}
+		if(tmp[i]>len)
+		{
+			len=tmp[i];
+		}
 	}
-
+	printf("len=%d\n",len);
 }
