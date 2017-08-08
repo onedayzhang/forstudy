8e7559e20607477eb0d2b8af71387cb59f5f41f5
root <oneday.zhang@gmail.com>
2017-08-08 20:05:20 +0800

    add
    Signed-off-by: root <oneday.zhang@gmail.com>

---
+++ tips/DP/LIS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc,char argv)
+{
+	int i,j;
+	int len=0;
+	int nums[8]={23,54,12,23,54,31,24,65,26}
+	int tmp[8]
+	for(i=0;i<8i++)
+	{
+		tmp[i]=1;
+		for(j=)
+	}
+
+}
