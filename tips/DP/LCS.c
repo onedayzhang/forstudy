42adbd8ba3451e7a800f0f8ef45fd475925da5ab
root <oneday.zhang@gmail.com>
2017-08-23 19:59:33 +0800

    start LCS
    Signed-off-by: root <oneday.zhang@gmail.com>

---
+++ tips/DP/LCS.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#define N 20
+
+int tmp[N][N]={0};
+
+int LCS(char *ch1,char *ch2)
+{
+	int lenth1,lenth2;
+	int i,j;
+	lenth1=strlen(ch1);
+	lenth2=strlen(ch2);
+	for(i=0;i<=lenth1;i++)
+		
+}
