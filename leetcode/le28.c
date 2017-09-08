7c10911d8a1f1459aa5002a61dff83ab68fb0b71
root <oneday.zhang@gmail.com>
2017-09-08 17:52:59 +0800

    update le28
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le28.c
+++ leetcode/le28.c
@@ -1,3 +1,29 @@
-int removeElement(int* nums, int numsSize, int val) {
-    
+#include <stdio.h>
+
+int strStr(char* haystack, char* needle) {
+	int i,j;
+	
+	int ret=-1;
+	if(haystack==NULL || needle==NULL)
+		return -1;
+	while(1)
+	{
+		while(*(haystack+i)!='\0'||*(haystack+i)!=*needle)
+		{
+			i++;
+		}
+		while(*(haystack+i+j)==*needle+j)
+		{
+			
+		}
+	}
+}
+
+int main(int argc,char* argv)
+{
+	if(argc>2)
+	{
+		pinrtf("%d\n",strStr(argv[1],argv[2]));
+	}
+	return 0;
 }
