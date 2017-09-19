1070c925a156bf26f323a8849e41f3b8a00dcbf4
root <oneday.zhang@gmail.com>
2017-09-19 20:03:01 +0800

    update
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le29.c
+++ leetcode/le29.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
-#define MAX_INT 0
+
 int divide(int dividend, int divisor) {
 	long long res=0;
-	
-	if(dividend<divisor)
-	return 0;
-	while(dividend>divisor)
+	if(!divisor||(dividend==INT_MIN&&divisor==-1))
+		return INT_MAX;
+	int sign=(dividend<0)^(divisor<0)? -1:1;
+	long long u_dividend =abs(dividend);
+	long long u_divisor=abs(divisor);
+	while(u_dividend>=u_divisor)
 	{
-		dividend-=divisor;
-		res++;
-		if(res>INT_MAX)
-		return MAX_INT;
+		long long tmp=u_divisor,multi=1;
+		while(u_dividend>=(tmp<<1))
+		{
+			tmp<<=1;
+			multi<<=1;
+		}
+		u_dividend-=tmp;
+		res+=multi;
 	}
+	return sign*res;
 }
 
 int main(int argc,char *argv[])
