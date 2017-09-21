a5bca3b2683f9686be411d847a15ab27346239dc
onedayzhang <oneday.zhang@gmail.com>
2017-09-21 08:07:25 -0700

    le35 is ok
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le35.c
+++ leetcode/le35.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+
 int searchInsert(int* nums, int numsSize, int target) {
-    
+
+	int i;
+	for(i=0;i<numsSize;i++)
+	{
+		if(nums[i]>=target)
+			break;
+	}
+	return i;
+}
+
+int main()
+{
+	int a[4]={1,3,5,6};
+	printf("%d\n",searchInsert(a,4,5));
+	return 0;
 }
