4c60d07e5f7cd01d0935647f3b3f56b12fdedb35
root <oneday.zhang@gmail.com>
2017-09-21 19:59:16 +0800

    le41 is ok
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le41.c
+++ leetcode/le41.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-
+#if 0
 int firstMissingPositive(int* nums, int numsSize) {
 	long long int tmp=0;
 	int i;
@@ -22,9 +22,32 @@ int firstMissingPositive(int* nums, int numsSize) {
 	}
 	return i;
 }
+#endif
+
+int firstMissingPositive(int* nums, int numsSize) {
+	int tmp;
+	int i;
+	for(i=0;i<numsSize;i++)
+	{
+		while(nums[i]>0&&nums[i]<=numsSize && nums[i]!=nums[nums[i] - 1])
+		{
+		//	swap(nums[i],nums[nums[i]-1]);
+			tmp=nums[i];
+			nums[i]=nums[tmp-1];
+			nums[tmp-1]=tmp;
+		}
+	}
+	for(i=0;i<numsSize;i++)
+	{
+		if(nums[i]!=(i+1))
+			break;
+	}
+    return (i+1);
+}
+
 
 int main()
 {
-	int a[65]={10,4,16,54,17,-7,21,15,25,31,61,1,6,12,21,46,16,56,54,12,23,20,38,63,2,27,35,11,13,47,13,11,61,39,0,14,42,8,16,54,50,12,-10,43,11,-1,24,38,-10,13,60,0,44,11,50,33,48,20,31,-4,2,54,-6,51,6};
-	printf("%d\n",firstMissingPositive(a,65));
+	int a[2]={1,1};
+	printf("%d\n",firstMissingPositive(a,2));
 }
