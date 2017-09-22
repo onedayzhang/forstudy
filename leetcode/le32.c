fa829b393b3688e2f08366f792e329bb6a3beabc
onedayzhang <oneday.zhang@gmail.com>
2017-09-22 08:12:14 -0700

    le32 is ok.but not very good
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- leetcode/le32.c
+++ leetcode/le32.c
@@ -1,3 +1,29 @@
+#include <stdio.h>
+#include <string.h>
 int longestValidParentheses(char* s) {
-    
+	int max=0;
+	int i,j;
+	int len=strlen(s);
+	int left,right;
+
+	for(j=0;j<len-max;j++)
+	{
+		left=0;
+		right=0;
+		for(i=j;i<len;i++)
+		{
+			if(s[i]=='(') left++;
+			else right++;
+			if(left<right) break;
+			if(left==right&&(left+right>max))
+				max=left+right;
+		}
+	}
+	return max;
+}
+
+int main(int argc,char* argv[])
+{
+	printf("%d\n",longestValidParentheses(argv[1]));
+	return 0;
 }
