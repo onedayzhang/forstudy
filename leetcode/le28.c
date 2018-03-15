41a48fdfaad9ecd2e795756572d3c8e143320a5a
root <oneday.zhang@gmail.com>
2018-03-15 17:25:42 +0800

    update kmp
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le28.c
+++ leetcode/le28.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#include <string.h>
 
-
-int KMP(char *s,char*p,int next[])
-{
-
-}
+#if 0
 int strStr(char* haystack, char* needle) {
 	int i=j=0;
 	int alen=strlen(haystack);
@@ -12,7 +9,7 @@ int strStr(char* haystack, char* needle) {
 
 	while(i<alen&&j<blen)
 	{
-		if(haystack[i]==needle[j]!!j=-1)
+		if(haystack[i]==needle[j]||j==-1)
 		{
 			i++;
 			j++;
@@ -28,28 +25,53 @@ int strStr(char* haystack, char* needle) {
 	}
 	return -1;
 }
-	
-	int ret=-1;
-	if(haystack==NULL || needle==NULL)
-		return -1;
-	while(1)
-	{
-		while(*(haystack+i)!='\0'||*(haystack+i)!=*needle)
-		{
-			i++;
-		}
-		while(*(haystack+i+j)==*needle+j)
-		{
-			
-		}
-	}
-}
 
 int main(int argc,char* argv)
 {
 	if(argc>2)
 	{
 		pinrtf("%d\n",strStr(argv[1],argv[2]));
+	}
+	return 0;
+}
+#endif
+
+int strStr(char* haystack, char* needle) {
+
+	int i,j;
+	char *a=haystack;
+	char *b=needle;
+	int alen=strlen(a);
+	int blen=strlen(b);
+	i=0;j=0;
+	while(i<alen &&j<blen)
+	{
+		if(a[i]==b[j]||j==-1)
+		{
+			i++;
+			j++;
+		}
+		else
+		{
+		//	j=b-next[a[j]];
+			j=next[j];
+		}
+	}
+	if(j==blen)
+		return (i-j);
+	else
+		return -1;
+
+}
+int getnext(char *target)
+{
+	
+}
+int main(int argc,char* argv[])
+{
+	if(argc>2)
+	{
+		printf("%d\n",strStr(argv[1],argv[2]));
 	}
 	return 0;
 }
