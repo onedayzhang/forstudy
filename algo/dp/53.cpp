08a97e1258d7b148699f497c11552961ec5218ed
onedayzhang <oneday.zhang@gmail.com>
2018-07-23 07:29:36 -0700

    update
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- algo/dp/53.cpp
+++ algo/dp/53.cpp
@@ -20,6 +20,15 @@ public:
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        
+        int n=nums.size();
+		vector<int> dp(n);
+		dp[0]=nums[0];
+        int result=nums[0];
+        for(int i=1;i<n;i++)
+		{
+			dp[i]=nums[i]+(dp[i-1]>0? dp[i-1]:0);
+			result=max(result,dp[i]);
+		}
+		return result;
     }
 };
