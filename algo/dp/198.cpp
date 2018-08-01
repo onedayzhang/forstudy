96175d5b9e4b8ab6a73f62c5bd7045496a9e643c
onedayzhang <oneday.zhang@gmail.com>
2018-08-01 07:50:09 -0700

    update
    Signed-off-by: onedayzhang <oneday.zhang@gmail.com>

--- algo/dp/198.cpp
+++ algo/dp/198.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        
+		if(nums.size()==0)
+			return 0;
+		if(nums.size()==1)
+			return nums[0];
+		if(nums.size()==2)
+			return max(nums[0],nums[1]);
+		vector<int> dp(nums.size());
+		int i;
+		dp[0]=nums[0];
+		dp[1]=max(nums[0],nums[1]);
+		for(i=2;i<nums.size();i++)
+		{
+			dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+		}
+		return dp[i-1];
     }
 };
