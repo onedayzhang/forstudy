ee607ccdc0060cad4d3814cc3904807b1d2d386b
root <oneday.zhang@gmail.com>
2018-10-11 20:06:09 +0800

    update
    Signed-off-by: root <oneday.zhang@gmail.com>

--- leetcode/le25.c
+++ leetcode/le25.c
@@ -8,3 +8,28 @@
 struct ListNode* reverseKGroup(struct ListNode* head, int k) {
     
 }
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+		ListNode* cur,next;
+		ListNode *pre=new ListNode(-1);
+		pre->next=head;
+		cur=head;
+		int num=0;
+		while(cur=cur->next) num++;
+		while(num>=k)
+		{
+			
+		}
+		
+    }
+};
